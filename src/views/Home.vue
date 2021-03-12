4c1d2ea805bbdc231d5a55a65091c15f53e6dfea
meylisday <victory.sett@gmail.com>
2021-03-12 21:19:35 +0300

    Init

---
+++ src/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="home">
+    <!-- <img alt="Vue logo" src="../assets/logo.png" /> -->
+    <p>{{ year }}</p>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" /> -->
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+// import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+
+export default defineComponent({
+  components: {
+    // HelloWorld
+  },
+  setup() {
+    const year = ref(2022);
+    return {
+      year
+    };
+  }
+});
+</script>
